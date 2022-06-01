0b0f17367a8475e88a6c85d3fdd94d071988c000
Doni Kalando <doni.kalandog@gmail.com>
2022-06-01 17:33:48 +0400

    added a new file with type .h to create and describe functions

--- Strukshen/Source.c
+++ Strukshen/Source.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include "Func.h"
 
 struct Car	// —труктура Car со своими типами данных, как шаблон.
 {
@@ -16,8 +17,8 @@ int main() {
 	bmw.weight = 2500.00f;
 
 	struct Car audi = {300, "Audu A8", 2700.00f};
-	printf("%s his speed: %d\n", audi.name, audi.speed);
-	printf("%s his speed: %d\n", bmw.name, bmw.speed);
+	printf("%s his speed: %d and weight %f\n", audi.name, audi.speed, audi.weight);
+	printf("%s his speed: %d and weight %f\n", bmw.name, bmw.speed, bmw.weight);
 
 	return 0;
 }
