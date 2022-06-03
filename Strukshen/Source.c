78779ac66ef3608cd8d21f445376a36511d02f52
Doni Kalando <doni.kalandog@gmail.com>
2022-06-03 20:39:13 +0400

    Добавил исключение для puts

--- Strukshen/Source.c
+++ Strukshen/Source.c
@@ -4,7 +4,9 @@
 
 int main() {
 
-	int rc = puts("Hello, MVStudio!");
+	int rs = puts("Hello, MVStudio!");
+	if (rs == EOF)
+		perror("puts()"); // POSIX требует, чтобы значение errno было установлено
 
 	struct Car bmw;	//Создали объект, на основе шаблона Car. Имеет доступ ко всем его параменным.
 
@@ -13,9 +15,10 @@ int main() {
 	bmw.weight = 3111.00f;
 
 	struct Car audi = { 300, "Audu A8", 2723.00f };
+
 	printf("%s his speed: %d and weight %f\n", audi.name, audi.speed, audi.weight);
 	printf("%s his speed: %d and weight %f\n", bmw.name, bmw.speed, bmw.weight);
-		
+
 	/* Calculator(); */ 
 	
 	return 0;
