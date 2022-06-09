2c7506978bb71cd07e7a97f1ea1ca93abf6e93e7
DKalando <doni.kalandog@gmail.com>
2022-06-10 00:08:11 +0400

    Создал консольную игру Арканоид

--- Strukshen/Source.c
+++ Strukshen/Source.c
@@ -1,6 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
+#include <string.h>
+#include <Windows.h>
+#include <math.h>
+#include <corecrt_math_defines.h>
 #include "Source.h"
 
 int main() {
@@ -9,23 +14,36 @@ int main() {
 	int rs = puts("Hello, MVStudio!");
 	if (rs == EOF)
 		perror("puts()"); // POSIX требует, чтобы значение errno было установлено
+		
+	float resus;
+	BOOL run = FALSE;	// Флажок, FALSHE по умолчанию
+	initRacket();		// Инициализируем Ракетку
+	initBall();			// Инициализируем Шарик
 
-	struct Car bmw;	//Создали объект, на основе шаблона Car. Имеет доступ ко всем его параменным.
+	do
+	{
+		setcur(0,0);
 
-	bmw.speed = 250;
-	strcpy(bmw.name, "BMW M5");	// для массивовов strcpy - позволяет копировать масив (в, из);
-	bmw.weight = 3111.00f;
+		if (run) avtoMoveBall();								// Если run = TRUE, то шарик в движении
+		else  moveBall(racket.x + racket.w / 2, racket.y - 1);	// Иначе шарик возвращается к Ракетке
+		if (ball.iy > height) {									// Если шарик ушел за ракетку (покинул поле), то вовзращается к ракетке
+			run = FALSE;
+			if (hitCnt > maxHitCnt) maxHitCnt = hitCnt;				// Максимальное колличество отражаенных Шариков от Ракетки
+			hitCnt = 0;												// Обнуляем счетчик
+		}
 
-	struct Car audi = { 300, "Audu A8", 2723.00f };
+		init();			// Инициализировали поле
+		putRacket();	// Вставляем Ракетку
+		putBall();		// Вставляем Шарик 
+		show();			// Отабражение поля
 
-	struct Abstract square = {5, 7};
+		if (GetKeyState('A') < 0) moveRacket(racket.x - 3);		// Двигать ракету в лево, если достигла стены не двигать дальше
+		if (GetKeyState('D') < 0) moveRacket(racket.x + 3);		// Двигать ракету в право, если достигла стены не двигать дальше
+		if (GetKeyState('W') < 0) run = TRUE;					// Перевод Флажок в TRUE 
 
-	calc(&square);
-
-	printf("Има машины: %s, её скорость: %d, её вес: %f\n", audi.name, audi.speed, audi.weight);
-	printf("Има машины: %s, её скорость: %d, её вес: %f\n\n", bmw.name, bmw.speed, bmw.weight);
-
-	/* Calculator(); */ 
+		printf("\nХ: %f:", resus = ball.x + cos(ball.alfa) * ball.speed);	// Отображение координаты Шарика по Х
+		printf("\nУ: %f:", resus = ball.y + sin(ball.alfa) * ball.speed);	// Отображение координаты Шарика по У
+	} while (GetKeyState(VK_ESCAPE) >= 0);					//Цыкл не закончится пока не будет нажат ESCAPE
 	
 	return 0;
 }
--- Strukshen/Source.h
+++ Strukshen/Source.h
@@ -1,62 +1,115 @@
 #pragma once
 
-struct Car	// Структура Car со своими типами данных, как шаблон.
+#define width 65
+#define height 25
+
+char mas[height][width + 1];		// Массив исспользуемый в виде игрового поля
+int hitCnt = 0, maxHitCnt = 0;		// Счетик отражаемых шаров и счетчик максимального колличества отраженных шариков
+
+typedef struct
 {
-	int speed;
-	char name[50];
-	float weight;
-};
+	int x, y, w;	// Координаты Ракетки X, У и длинна W
+} TRacket; //Тип Ракета
 
-struct Abstract
-{
-	float width, height;
-};
+typedef struct {
+	float x, y;		// Координаты Шарика типа float
+	int ix, iy;		// Координаты Шарика типа int
+	float alfa;		// 
+	float speed;	// Скорость шарика
+} TBall; //Тип Шарик
 
-void calc(struct Abstract *obj) {
-	float res = obj->height * obj->width;
-	printf("Result: %.2f\n", res);
-};
+TRacket racket;	//Ракета типа Ракеты
+TBall ball;		//Шарик типа Шарика
+TBall ballSave;	//Копия шарика со старыми значениями
 
-/*float operac(float num_first, float num_second, char znak) {
-	float res = 0;
+void initRacket() {	//Инициализатор Ракеты
+	racket.w = 7;
+	racket.x = (width - racket.w) / 2;
+	racket.y = height - 1;
+}
 
-	switch (znak)
-	{
-	case '+':
-		res = num_first + num_second;
-		break;
-	case '-':
-		res = num_first - num_second;
-		break;
-	case '*':
-		res = num_first * num_second;
-		break;
-	case '/':
-		res = num_first / num_second;
-		break;
-	default:
-		printf("Символ не соответствует!\n");
-		break;
+void initBall() {	//Инициалтзатор шарика
+	ball.x = 2;
+	ball.y = 2;
+	ball.alfa = -1;
+	ball.speed = 2,0;
+}
+
+void putRacket() {	//Всталение ракеты на поле
+	for (int i = racket.x; i < racket.x + racket.w; i++) mas[racket.y][i] = '@';
+}
+
+void putBall() {	//Вставление шарика на поле
+	mas[ball.iy][ball.ix] = '*';
+}
+
+void init() {		//Инициализация поля
+	for (int i = 0; i < width; i++) mas[0][i] = '#';
+	
+	mas[0][width] = '\0';
+	strncpy(mas[1], mas[0], width + 1);
+
+	for (int i = 1; i < width - 1; i++) mas[1][i] = ' ';
+	for (int i = 2; i < height; i++) strncpy(mas[i], mas[1], width + 1);
+}
+
+void show() {		//Отображения поля
+	for (int i = 0; i < height; i++) {
+		printf("%s", mas[i]);
+
+		if (i == 3) printf(" hit %i", hitCnt);
+		if (i == 4) printf(" max %i", maxHitCnt);
+		if (i < height - 1) printf("\n");
+	}
+}
+
+void moveRacket(int x) {	//Перемещение ракеты
+	racket.x = x;
+	if (racket.x < 1) racket.x = 1;
+	if (racket.x + racket.w >= width) racket.x = width - 1 - racket.w;
+}
+
+void moveBall(float x, float y) {	//Передвижение шарика
+	ball.x = x;
+	ball.y = y;
+	ball.ix = (int) ball.x;
+	ball.iy = (int) ball.y;
 	}
 
-	return res;
-};
+void avtoMoveBall() {
+	if (ball.alfa < 0) ball.alfa += M_PI * 2;			//Ограничели движения шарика от 0
+	if (ball.alfa > M_PI * 2) ball.alfa -= M_PI * 2;	//Ограничели движения шарика до Пи
+	
+	ballSave = ball;
 
-void Calculator() {
-	float a = 0, b = 0;
-	char z = '0';
+	moveBall(ball.x + cos(ball.alfa) * ball.speed, ball.y + sin(ball.alfa) * ball.speed);
 
-	printf("Формат ввода: (число, знак операции, число)\n");
-	printf("знаки операции: ( + ), ( - ), ( / ), ( * )\n");
+	if ((mas[ball.iy][ball.ix] == '#') || (mas[ball.iy][ball.ix] == '@')) {	// Если Шарик сталкнулся со стеной или ракетой
+		if (mas[ball.iy][ball.ix] == '@') hitCnt++;								// + к колличеству ударов об ракетку
+		if ((ball.ix != ballSave.ix) && (ball.iy != ballSave.iy)) {				// Сравниваем старое положение шарика с новым
+			if (mas[ballSave.iy][ball.ix] == mas[ball.iy][ballSave.ix])			// Если шарик в углу, то меняем направление в противоположную сторону
+				ballSave.alfa = ballSave.alfa + M_PI;
+			else																// ИНаче ...
+			{
+				if (mas[ballSave.iy][ball.ix] == '#')								// Если поверхность вертикальная, то ...  
+					ballSave.alfa = (2 * M_PI - ballSave.alfa) + M_PI;				// Отражение по вертикали
+				else																// Иначе ...
+					ballSave.alfa = (2 * M_PI - ballSave.alfa);						// Отражение по гаризонтали
+			}																	// Иначе ...
+		}																	// Иначе ...
+		else if (ball.iy == ballSave.iy)									// Если Y не изменялся то ... 
+			ballSave.alfa = (2 * M_PI - ballSave.alfa) + M_PI;					// Отражение по вертикали
+		else																// Иначе ...
+			ballSave.alfa = (2 * M_PI - ballSave.alfa);							// Отражение по гаризонтали
 
-	scanf("%f %s %f", &a, &z, &b);
-
-	while (b == 0) {
-		printf("На 0 делить нельзя!\n Ввите другое число: ");
-		scanf("%f", &b);
+		ball = ballSave;
+		avtoMoveBall(); 
 	}
+}
 
-	printf("Результат: %f\n", operac(a, b, z));
-
-}; */
-
+void setcur(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
